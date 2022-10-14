e06d2c2914503277c6fbc183f8c96b830ab5fa85
HYEBPARK <hyeqls3742@gmail.com>
2022-10-14 22:10:55 +0900

    init : 프로젝트 생성

---
+++ src/main/java/org/swlab/ticketingservice/TicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.swlab.ticketingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/swlab/ticketingservice/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swlab.ticketingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
