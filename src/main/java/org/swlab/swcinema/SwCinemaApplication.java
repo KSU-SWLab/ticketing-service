2e6705ab80f4fd2e825910c17c5c35b3bed85332
HYEBPARK <hyeqls3742@gmail.com>
2022-10-21 20:50:07 +0900

    refactor : application 네이밍 변경

--- src/main/java/org/swlab/swcinema/SwCinemaApplication.java
+++ src/main/java/org/swlab/swcinema/SwCinemaApplication.java
@@ -1,13 +1,13 @@
-package org.swlab.ticketingservice;
+package org.swlab.swcinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingServiceApplication {
+public class SwCinemaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicketingServiceApplication.class, args);
+		SpringApplication.run(SwCinemaApplication.class, args);
 	}
 
 }
